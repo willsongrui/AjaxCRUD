291434ab28e44be557608ab6bee3e088817456e5
songruirui <willsongrui@gmail.com>
2013-08-07 21:45:32 +0800

    pure ajax

---
+++ Documents/codes/rails/rails_projects/AjaxCRUD/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxCRUD::Application.config.secret_token = '1080ab282184ad577f53cb2bc60b2506c82a79ab00c763f19ec15925b173a49b38a630a682d71ba1ca084b4437c0c314425f9bc48ecd0c501eee9fdc389a652f'
